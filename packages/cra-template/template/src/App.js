ccf01110acdd40470fc7ecb2cfe22e71626f74d1
belgranomanuela <53871616+belgranomanuela@users.noreply.github.com>
2020-09-23 13:01:38 -0300

    cambios antes de nuevo create react app

--- packages/cra-template/template/src/App.js
+++ packages/cra-template/template/src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar.jsx'
 
 function App() {
   return (
     <div className="App">
-      <h1>Amigos Palacio Belgrano Próximamente</h1>
+      <Navbar />
+      <header className="App-header">
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Amigos Palacio Belgrano
+        </a>
+      </header>
+
     </div>
+
   );
 }
 export default App;
